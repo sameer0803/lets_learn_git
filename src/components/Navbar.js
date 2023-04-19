14eabb511ca8638f880199f85a05ed07c681c911
Sameer Uddin <Samudd246@gmail.com>
2023-04-19 15:37:15 +0530

    initial commit 3

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ export default function Navbar(props) {
     <nav className = {`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
       
     <div className="container-fluid">
-      <a className="navbar-brand" href="/">{props.title}</a>
+      <a className="navbar-brand" href="#">{props.title}</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
@@ -19,17 +19,17 @@ export default function Navbar(props) {
             <a className="nav-link active" aria-current="page" href="/">Home</a>
           </li> 
           <li className="nav-item">
-            <a className="nav-link" href="/about">{props.aboutText}</a>
+            <a className="nav-link" href="#">{props.aboutText}</a>
           </li>
           <li className="nav-item dropdown">
             <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Dropdown
             </a>
             <ul className="dropdown-menu">
-              <li><a className="dropdown-item" href="/">Action</a></li>
-              <li><a className="dropdown-item" href="/">Another action</a></li>
+              <li><a className="dropdown-item" href="#">Action</a></li>
+              <li><a className="dropdown-item" href="#">Another action</a></li>
               <li><hr className="dropdown-divider"/></li>
-              <li><a className="dropdown-item" href="/">Something else here</a></li>
+              <li><a className="dropdown-item" href="#">Something else here</a></li>
             </ul>
           </li>
          
